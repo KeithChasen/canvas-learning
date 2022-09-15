92cd488117af24d2d982349426894209d4348187
keithchasen <keithchasen89@gmail.com>
2022-09-15 08:01:10 +0300

    simple listener for mouse coordinates

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth * .9;
-canvas.height = window.innerHeight * .9;
+const canvasWidth = window.innerWidth * .9
+const canvasHeight = window.innerHeight * .9
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+const minX = Math.round((window.innerWidth - canvasWidth) / 2);
+const maxX = Math.round(window.innerWidth - (window.innerWidth - canvasWidth) / 2);
+
+const minY = Math.round((window.innerHeight - canvasHeight) / 2);
+const maxY = Math.round(window.innerHeight - (window.innerHeight - canvasHeight) / 2);
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
 
 ctx.fillStyle = 'green';
 ctx.fillRect(40, 50, 100, 100);
+
+const coordinate = { x: 0, y: 0 };
+
+document.onmousemove = e => keyCallback(e);
+
+const keyCallback = e => {
+    if (e.clientX > minX && e.clientX < maxX && e.clientY > minY && e.clientY < maxY) {
+        coordinate.x = e.clientX;
+        coordinate.y = e.clientY;
+    }
+    console.log(coordinate, 'coordinate')
+}
