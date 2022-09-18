7b3e12c723869ac691bbf27cc0c7c759259c710e
keithchasen <keithchasen89@gmail.com>
2022-09-18 20:13:00 +0300

    cleaned extra code

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const canvasWidth = window.innerWidth * .9
-const canvasHeight = window.innerHeight * .9
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
-
-const minX = Math.round((window.innerWidth - canvasWidth) / 2);
-const maxX = Math.round(window.innerWidth - (window.innerWidth - canvasWidth) / 2);
-
-const minY = Math.round((window.innerHeight - canvasHeight) / 2);
-const maxY = Math.round(window.innerHeight - (window.innerHeight - canvasHeight) / 2);
-
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 ctx.fillStyle = 'green';
 ctx.fillRect(40, 50, 100, 100);
+ctx.save();
+ctx.translate(150, 150);
 
 const coordinate = { x: 0, y: 0 };
 
 document.onmousemove = e => keyCallback(e);
 
 const keyCallback = e => {
-    if (e.clientX > minX && e.clientX < maxX && e.clientY > minY && e.clientY < maxY) {
-        coordinate.x = e.clientX;
-        coordinate.y = e.clientY;
-    }
+    coordinate.x = e.clientX;
+    coordinate.y = e.clientY;
     console.log(coordinate, 'coordinate')
 }
